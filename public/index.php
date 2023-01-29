164a8e51905e7acc77f2382fa154df83830809d1
David Green <davidgr33n@mail.com>
2023-01-29 23:33:10 +0200

    finish basic routing functionlaty including not found page!

--- public/index.php
+++ public/index.php
@@ -5,9 +5,21 @@ require '../src/router.php';
 use BlockBuster\Router;
 
 
-$router = new Router();
+$router = new BlockBuster\Router();
 
-$router->get('get', function(){
+$router->get('/', function(){
     
     echo 'home page!';
 });
+
+$router->get('/about', function(){
+    
+    echo 'about us';
+});
+
+$router->Handler404( function(){
+    echo'<h1>Not Found Page!!</h1>';
+});
+
+
+$router->run();
--- src/router.php
+++ src/router.php
@@ -9,15 +9,74 @@ namespace BlockBuster;
  */
 class Router {
     
-    public function get() {
+    private $handlers = array();
+    public $notFoundHandler;
+
+
+    public function get(string $path, $handler): void {
         
+        $this->addHandler($path, 'GET', $handler);
     }
+    
+    
+    public function post(string $path, $handler): void {
+        
+        $this->addHandler($path, 'POST', $handler);
+    }
+    
     
     public function set() {
         
     }
     
-    public function run() {
+    
+    public function run(): void {
+        
+        $requestUrl = parse_url($_SERVER['REQUEST_URI']);
+        $requestPath = $requestUrl['path'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        
+        $callBack = null;
+        
+        foreach ($this->handlers as $config){
+            
+            
+            if($config['path'] === $requestPath && $method === $config['method']){
+                
+                $callBack = $config['handler'];
+            }
+        }
+        
+        if(!$callBack){
+            header('HTTP/1.0 404 Not Found');
+            
+            if( !empty($this->notFoundHandler) ){
+                
+                $callBack = $this->notFoundHandler;
+            }
+            
+        }
+        
+        call_user_func($callBack);
+    }
+    
+    
+    public function Handler404( $callBack ):void{
+        
+        $this->notFoundHandler = $callBack;
+    }
+
+
+
+
+    private function addHandler(string $path, string $method ,$handler){
+        
+        $this->handlers[$method. $path] = [
+            
+            'path' => $path,
+            'method' => $method,
+            'handler' => $handler
+        ];
         
     }
     
